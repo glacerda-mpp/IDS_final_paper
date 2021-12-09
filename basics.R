e3c1a2a1457de4f7566f2413e03f1c2571bf3134
RodrigoBolanosS <r.bolanos@mpp.hertie-school.org>
2021-12-09 10:42:51 +0100

    fff

--- basics.R
+++ basics.R
@@ -1,6 +1,12 @@
 
+#load packages
+library(pdftools)
+library(tidyverse)
+library(stringr)
 
-#diversity statement function
+
+##1. Diversity statement function##
+
 read_div_statement <- function(syllabus_pdf){
   
   #Cut it by lines
@@ -33,10 +39,11 @@ read_div_statement <- function(syllabus_pdf){
   
   div_statement<- div_statement %>%str_remove(.,"[:digit:]") %>% str_trim(.)
   
+  div_statement
   
 }
 
-#readings function 
+#@ readings function 
 
 get_readings <- function(syllabus_pdf){
   
@@ -90,4 +97,72 @@ get_readings <- function(syllabus_pdf){
   
 }
 
+working_directory<-getwd()
+
+list_syllabi<-list.files("./syllabi_pdfs")
+
+
+bauer_io_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[1]))
+ahrens_ec_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[2]))  
+gruener_fmp_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[3]))
+lancker_ec2_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[4]))
+patz_gg_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[5]))
+jacthen_gg_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[6]))
+cali_gg_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[7]))
+econ_ciril_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[8]))
+lg_bobi_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[9]))
+inter_sec_gohdes_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[10]))
+law_dkt_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[11]))
+welfare_pp_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[12]))
+climate_hf_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[13]))
+process_graf_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[14]))
+processeu_migliorati_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[15]))
+public_hustedt_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[16]))
+public_parrado_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[17]))
+public_wegrich_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[18]))
+stats_kayser_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[19]))
+stats_munzert_pdf<-pdf_text(paste0(working_directory,"/syllabi_pdfs/",list_syllabi[20]))
+
+read_div_statement(bauer_io_pdf)
+read_div_statement(ahrens_ec_pdf)
+read_div_statement(gruener_fmp_pdf) #no diversity statmeent (poner el error)
+read_div_statement(lancker_ec2_pdf)
+read_div_statement(patz_gg_pdf)
+read_div_statement(jacthen_gg_pdf)
+read_div_statement(cali_gg_pdf)
+read_div_statement(econ_ciril_pdf)
+read_div_statement(lg_bobi_pdf) #no diversity statement
+read_div_statement(inter_sec_gohdes_pdf)
+read_div_statement(law_dkt_pdf) #si hay pero es en minúsculas
+read_div_statement(welfare_pp_pdf) #no diversity statement
+read_div_statement(climate_hf_pdf) #si hay pero es en minúsculas
+read_div_statement(process_graf_pdf)
+read_div_statement(processeu_migliorati_pdf)
+read_div_statement(public_hustedt_pdf)
+read_div_statement(public_parrado_pdf)
+read_div_statement(public_wegrich_pdf)
+read_div_statement(stats_kayser_pdf) #no diversity statemente
+read_div_statement(stats_munzert_pdf)
+
+
+get_readings(bauer_io_pdf) #subscript out of bounds
+get_readings(ahrens_ec_pdf)
+get_readings(gruener_fmp_pdf) 
+get_readings(lancker_ec2_pdf)
+get_readings(patz_gg_pdf)
+get_readings(jacthen_gg_pdf) #subscript out of bounds
+get_readings(cali_gg_pdf) #subscript out of bounds
+get_readings(econ_ciril_pdf)
+get_readings(lg_bobi_pdf) 
+get_readings(inter_sec_gohdes_pdf) #subscript out of bounds
+get_readings(law_dkt_pdf) 
+get_readings(welfare_pp_pdf)  #subscript out of bounds
+get_readings(climate_hf_pdf) #subscript out of bounds
+get_readings(process_graf_pdf) #subscript out of bounds
+get_readings(processeu_migliorati_pdf) #subscript out of bounds
+get_readings(public_hustedt_pdf) 
+get_readings(public_parrado_pdf)
+get_readings(public_wegrich_pdf)
+get_readings(stats_kayser_pdf) #subscript out of bounds
+get_readings(stats_munzert_pdf)
 
